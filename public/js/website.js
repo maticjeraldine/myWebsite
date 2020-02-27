baa76b262e97f2d8521937f423621a827b884f84
killer-123 <jeraldinecitam@gmail.com>
2020-02-27 10:51:59 +0800

    design changes

---
+++ public/js/website.js
@@ -0,0 +1,29 @@
+// STICKY HEADER
+window.onscroll = function () { myFunction() };
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+
+
+// MAP
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    var uluru = { lat: 14.597, lng: 120.956 };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 4, center: uluru });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({ position: uluru, map: map });
+}
+
+
--- resources/views/auth/navbar.blade.php
+++ resources/views/auth/navbar.blade.php
@@ -3,24 +3,23 @@
     <div class="float-left" style="margin-right:auto">
         <div class="dropdown">
 
-            <button class="dropbtn btn p-2 border-light" style="background-color:#d5c455">
+            <button class="dropbtn btn p-2 border-light" style="background-color:#FF6584">
                 <i class="fas fa-bars"></i> &nbsp; Menu</button>
-            <div class="dropdown-content" style="background-color:#d8c962">
-                <a href="#about" class="border border-bottom-dark">About Me</a>
-                <a href="#education" class="border border-bottom-dark">Education</a>
-                <a href="#achievements" class="border border-bottom-dark">Achievements</a>
-                <a href="#skills" class="border border-bottom-dark">Skills</a>
-                <a href="#projects" class="border border-bottom-dark">Projects</a>
-                <a href="#photo" class="border border-bottom-dark">Photo Galery </a>
-                <a href="#whyMe" class="border border-bottom-dark">Why Me?</a>
-                <a href="#charRef" class="border border-bottom-dark">Character Reference</a>
-                <a href="#contact" class="border border-bottom-dark">Contact</a>
+            <div class="dropdown-content" style="background-color:#FF6584">
+                <a id="btnAbout" class="border border-bottom-dark">About Me</a>
+                <a id="btnEduc" class="border border-bottom-dark">Education</a>
+                <a id="btnAchieve" class="border border-bottom-dark">Achievements</a>
+                <a id="btnSkills" class="border border-bottom-dark">Skills</a>
+                <a id="btnProjects" class="border border-bottom-dark">Projects</a>
+                <a id="btnPhotos" class="border border-bottom-dark">Photo Galery </a>
+                <a id="btnWhyMe" class="border border-bottom-dark">Why Me?</a>
+                <a id="btncharRef" class="border border-bottom-dark">Character Reference</a>
+                <a id="btnContact" class="border border-bottom-dark">Contact</a>
 
             </div>
         </div>
-        {{-- <button class="dropbtn btn p-2 border-light" style="background-color:#d5c455">
-            <a href="#landing"> <i class="fas fa-home text-dark"></i></button></a> --}}
-        <button class="dropbtn btn p-2 border-light" style="background-color:#d5c455" title="Get the copy of my resume">
+
+        <button class="dropbtn btn p-2 border-light" style="background-color:#FF6584" title="Get the copy of my resume">
             <a href="{{ asset('images/Matic,Jeraldine.pdf') }}" download="" class="text-dark blinking">
                 <span class="font-weight-bold">HIRE ME &nbsp; <i class="fas fa-download"></i> </span></button></a>
 
